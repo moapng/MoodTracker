6ada3ee46246b6595391c4e14304d2ec08b1e907
moa <larssonmoaml@gmail.com>
2021-08-19 19:35:03 +0200

    xx

--- mood-tracker/src/components/menu.js
+++ mood-tracker/src/components/menu.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch, Link } from 'react-router-dom';
 import CardComponent from './cardComponent';
 
 class Menu extends Component {
     render() {
         return (
             <div>
-                <div class="row">
-                    <CardComponent src="../icons/cloudy.png" alt="Image of a sun and a cloud" name="weather" />
-                    <CardComponent src="../icons/mood.icon.png" alt="Image of a few different moods; happy, sad, angry" name="mood" />
-                </div>
-                <div class="row">
-                    <CardComponent src="../icons/checkbox2.png" alt="Image of checkboxes" name="habits" />
-                    <CardComponent src="../icons/note.png" alt="Image of a generic note" name="things of note" />
-                </div>
+                <Router>
+                    <div class="row">
+                        <Link to="/weather">
+                            <CardComponent src="../icons/cloudy.png" alt="Image of a sun and a cloud" name="weather" />
+                        </Link>
+                        <Link to="/mood">
+                            <CardComponent src="../icons/mood.icon.png" alt="Image of a few different moods; happy, sad, angry" name="mood" />
+                        </Link>
+                    </div>
+                    <div class="row">
+                        <Link to="/habits">
+                            <CardComponent src="../icons/checkbox2.png" alt="Image of checkboxes" name="habits" />
+                        </Link>
+                        <Link to="/things-of-note">
+                            <CardComponent src="../icons/note.png" alt="Image of a generic note" name="things of note" />
+                        </Link>
+                    </div>
+                </Router>
             </div>
         );
     }
