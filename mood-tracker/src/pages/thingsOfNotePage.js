75da401ae9457d8adbe5f126c0bdfa8202cbec6e
moa <larssonmoaml@gmail.com>
2021-09-20 23:04:56 +0200

    post not working
    says enable CORS, but cors is enabled. idk

--- mood-tracker/src/pages/thingsOfNotePage.js
+++ mood-tracker/src/pages/thingsOfNotePage.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import { BrowserView, MobileView } from 'react-device-detect';
-import BrowserWindowComponent from '../components/browserWindowComponent';
 import MobileWindowComponent from '../components/mobileWindowComponent';
 
 class ThingsOfNotePage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            noteData: '',
+            nDate: '',
+            id: ''
+
+        }
+    }
+    handleSubmit() {
+        fetch('https://localhost:44302/api/ofnote', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+            body: JSON.stringify({
+                noteData: this.state.noteData,
+                nDate: this.state.nDate,
+                id: this.state.id
+            }),
+        }
+        )
+        // .then(response => response.json())
+        // .then(data => { console.log(data); })
+        // .catch(error => { console.log('Error:', error); })
+    }
     render() {
         return (
             <>
@@ -11,17 +34,13 @@ class ThingsOfNotePage extends Component {
                     <div className="row" />
                     <form>
                         <div className="form-group">
-                            <label for="noteTextArea">Things of Note</label>
-                            <textarea className="form-control" id="noteTextArea" rows="8" placeholder="Write down anything you'd like!"></textarea>
+                            <label htmlFor="noteTextArea">Things of Note</label>
+                            <textarea onChange={(e => this.setState({ noteData: e.target.value }))} className="form-control" id="noteTextArea" rows="8" placeholder="Write down anything you'd like!"></textarea>
                             <div className="row" />
-                            <button type="submit" className="btn btn-primary" id="noteSubmit">Submit</button>
+                            <button type="submit" className="btn btn-primary" id="noteSubmit" onSubmit={this.handleSubmit()}>Submit</button>
                         </div>
                     </form>
 
-
-                    {/* <BrowserWindowComponent title="Things of Note" info="anything fun happen today???">
-                        
-                    </BrowserWindowComponent> */}
                 </BrowserView>
 
                 <MobileView>
