9f2f9bd9dfd439abd343e52c97169209bf4aa7dc
moa <larssonmoaml@gmail.com>
2021-11-15 22:34:08 +0100

    habitspage v1

--- mood-tracker/src/pages/habitsPage.js
+++ mood-tracker/src/pages/habitsPage.js
@@ -6,7 +6,36 @@ function HabitsPage() {
     return (
         <>
             <BrowserView>
-
+                <div className="container">
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="toothbrushBox" value="toothbrush" />
+                        <label className="form-check-label" for="toothbrushBox">toothbrush</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="hairbrushBox" value="hairbrush" />
+                        <label className="form-check-label" for="toothbrushBox">hairbrush</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="skincareBox" value="skincare routine" />
+                        <label className="form-check-label" for="skincareBox">skincare routine</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="plantBox" value="plants" />
+                        <label className="form-check-label" for="plantBox">plants</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="gymBox" value="gym" />
+                        <label className="form-check-label" for="gymBox">gym</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="dailyCleaningBox" value="daily cleaning" />
+                        <label className="form-check-label" for="dailyCleaningBox">daily cleaning</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox" id="medBox" value="meds" />
+                        <label className="form-check-label" for="medBox">meds</label>
+                    </div>
+                </div>
             </BrowserView>
 
             <MobileView>
